602788feb3e0a956c6f4e9e5fb666aa0390c8dd9
hweiei <64945317+hweiei@users.noreply.github.com>
2020-08-28 00:41:12 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,6 @@
 var config = {
        texts: [
-        "送给你": "./imgs/02.png"     
-        "一个大大的惊喜~", 
-        "今天是你的生日~": "./imgs/03.png",
-        "愿你今后",: "./imgs/03.png",
-        "睡好吃好玩好~",: "./imgs/03.png",
-        "无忧无虑",: "./imgs/03.png",
-        "今天，你有吃蛋糕吗.": "./imgs/01.png",
-        "没有的话，快点去哦~": "./imgs/04.png",
-        "爱你",
+          "Happy Birthday",
 
     ],   
     desc: {
